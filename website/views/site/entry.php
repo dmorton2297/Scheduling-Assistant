886525f50369a853446117bd1c124f03eab30d90
Dan Morton <dmorton2297@gmail.com>
2016-06-03 17:40:14 -0400

    updated CCalEntryForm and fixed SiteController. STABLE

--- website/views/site/entry.php
+++ website/views/site/entry.php
@@ -3,11 +3,4 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 ?>
 
-<?php $form = ActiveForm::begin(['id' => 'create-calendar-entry-form']); ?>
-<?= $form->field($model, 'title') ?>
-<?= $form->field($model, 'description') ?>
-<div class="form-group">
-    <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
-</div>
-
-<?php ActiveForm::end(); ?>
+<p> You have made it here </p>
