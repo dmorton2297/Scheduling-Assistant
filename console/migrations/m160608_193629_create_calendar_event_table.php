849fa36cf4c392c3b540a8c82d1fe24301ee8ae5
Dan Morton <dmorton2297@gmail.com>
2016-06-09 00:38:50 -0400

    Finishged migration and reated calendar_event table in the sql database

--- console/migrations/m160608_193629_create_calendar_event_table.php
+++ console/migrations/m160608_193629_create_calendar_event_table.php
@@ -1,7 +1,7 @@
 <?php
 
 use yii\db\Migration;
-
+USE yii\db\Schema;
 /**
  * Handles the creation for table `calendar_event_table`.
  */
@@ -14,7 +14,15 @@ class m160608_193629_create_calendar_event_table extends Migration
     {
         $this->createTable('calendar_event_table', [
             'id' => $this->primaryKey(),
+            'user_id' => Schema::TYPE_INTEGER,
+            'title' => Schema::TYPE_STRING . '(32) NOT NULL',
+            'descrption' => Schema::TYPE_STRING, 
+            'notes' => Schema::TYPE_STRING, 
+            'start_time' => Schema::TYPE_INTEGER, 
+            'end_time' => Schema::TYPE_INTEGER,
         ]);
+
+        $this->addForeignKey("fk_user", "calendar_event_table", "user_id", "users", "id", "CASCADE", "RESTRICT");
     }
 
     /**
